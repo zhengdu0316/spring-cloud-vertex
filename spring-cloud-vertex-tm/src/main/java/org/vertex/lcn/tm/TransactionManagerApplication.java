5acce0a7561276934ea2de6f0196b2fb91d5f3ef
zhengdu <zhengdu1992@163.com>
2019-05-08 22:36:53 +0800

    TM

---
+++ spring-cloud-vertex-tm/src/main/java/org/vertex/lcn/tm/TransactionManagerApplication.java
@@ -0,0 +1,18 @@
+package org.vertex.lcn.tm;
+
+import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author zhengdu
+ * @date 2019/5/8 下午9:03
+ */
+@SpringBootApplication
+@EnableTransactionManagerServer
+public class TransactionManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionManagerApplication.class, args);
+    }
+}
